a53d3d93cb2d5252117dbbe340ea787446c917ea
Kirti Pratihar <167183967+KirtiPratihar@users.noreply.github.com>
2026-05-30 13:19:53 +0530

    Add comments for unordered_map usage in cpp/unordered_mp.cpp
    Added comments explaining the usage of unordered_map in C++.

---
+++ cpp/unordered_mp.cpp
@@ -0,0 +1,11 @@
+// see in CPP HashMap or hashtable are the same thing . 
+// To initialize the code is-
+unordered_map<int, int> map;
+
+// to put a value
+map.insert({k,l});
+or
+map[k]=l;
+
+//to erase a value
+map.erase(k);
