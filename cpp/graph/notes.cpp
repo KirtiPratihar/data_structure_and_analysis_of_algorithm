dfc4e13addfe42e774e64b3b32d2f54f979a8264
Kirti Pratihar <167183967+KirtiPratihar@users.noreply.github.com>
2026-06-19 18:38:10 +0530

    Add comments to notes.cpp for graph concepts
    Added comments explaining graph concepts and terminology.

---
+++ cpp/graph/notes.cpp
@@ -0,0 +1,10 @@
+// edges are the joining of 2 vertices
+//path - contains nodes, and each of them is connected
+//a node can't appear twice in a path
+// degree- no. of edges connected to that node
+// degree = 2* node
+//directed graph - indegree(node) - no. of edges pointing toward that specific node.
+
+
+//Graph Representation -
+// n- node, m- edges
