ab87cffc7417dd6ac9d0f8d41432266fdd6ab33f
Kirti Pratihar <167183967+KirtiPratihar@users.noreply.github.com>
2026-07-07 12:35:05 +0530

    Add DFS implementation for graph traversal
    Implement depth-first search (DFS) for graph traversal.

--- cpp/graph/notes.cpp
+++ cpp/graph/notes.cpp
@@ -8,3 +8,36 @@
 
 //Graph Representation -
 // n- node, m- edges
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void dfs(vector<vector<int>>& graph,vector<bool>& seen,int node){
+    seen[node]=true;
+    cout<<node<<endl;
+    for(int i:graph[node]){
+        if(!seen[i]){
+            dfs(graph,seen,i);
+        }
+    }
+    
+}
+
+
+int main(){
+    int ver, edge;
+    cin>>ver>>edge;
+    vector<vector<int>> graph(ver+1);
+    for(int i=0;i<edge;i++){
+        int u,v;
+        cin>>u>>v;
+        graph[u].push_back(v);
+    }
+    vector<bool> seen(ver+1,false);
+    vector<vector<int>> result;
+    vector<int> curr;
+    dfs(graph,seen,1);
+    return 0;
+}
