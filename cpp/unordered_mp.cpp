6c34cd5dc515ec7a3f62be6dbbe3bcacb577a588
Kirti Pratihar <167183967+KirtiPratihar@users.noreply.github.com>
2026-08-16 16:27:20 +0530

    Implement word count and repetition check
    Added functionality to count word occurrences in a string and print words with repetition greater than 2.

--- cpp/unordered_mp.cpp
+++ cpp/unordered_mp.cpp
@@ -9,3 +9,31 @@ map[k]=l;
 
 //to erase a value
 map.erase(k);
+
+//print all the words and there repetation.
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include<unordered_map>
+using namespace std;
+
+
+int main(){
+    string s,word;
+    getline(cin,s);
+    unordered_map<string , int> mp;
+    stringstream ss(s);
+    while(ss>>word){
+        mp[word]+=1;
+        }
+    for(auto const&pair :mp){
+        cout<<pair.first<<":"<<pair.second;
+        if(pair.second>=2){
+            cout<<endl<<"this is more than 2"<<pair.first;
+            }
+        }
+    
+    
+    
+    return 0;}
